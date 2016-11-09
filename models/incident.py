37726e076bb8c265b8857f47a6315bb994d9b56e
Alexander DeRieux <acd1797@vt.edu>
2016-11-09 17:45:52 -0500

    Created basic structure for an incident.

---
+++ models/incident.py
@@ -0,0 +1,17 @@
+
+class Incident(object):
+	"""docstring for Incident"""
+	def __init__(self, ID, description, address, latitude, longitude, time, title, organizations):
+		self.id = ID
+		self.description = description
+		self.address = address
+		self.latitude = latitude
+		self.longitude = longitude
+		self.time = time
+		self.title = title
+		self.organizations = organizations
+
+
+
+
+
