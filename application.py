44fdd99d0e9b63bc9f61c455920b58939e9b3c62
Alexander DeRieux <acd1797@vt.edu>
2016-11-09 19:24:02 -0500

    Correctly pushes new incidents to the database.

--- application.py
+++ application.py
@@ -9,8 +9,8 @@ from boto.dynamodb2.table 	import Table
 import os
 
 # Set environment variables.
-os.environ["AWS_ACCESS_KEY_ID"] = ""
-os.environ["AWS_SECRET_ACCESS_KEY"] = ""
+os.environ["AWS_ACCESS_KEY_ID"] = "AKIAJPSBNOVF7M4MZULA"
+os.environ["AWS_SECRET_ACCESS_KEY"] = "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"
 
 # Setup Flask
 app = Flask(__name__)
@@ -45,6 +45,20 @@ def incidents():
 		if request.form['submit'].lower() == "refresh":
 			INCIDENTS.append(Incident("id","hello there","address","latitude","longitude","time","Incident #1",None))
 			INCIDENTS.append(Incident("id","oh goodness!","address","latitude","longitude","time","Incident #2",None))
+			dynamo.tables['emergencyresponderin-mobilehub-146580548-Scenes'].put_item(
+				data={
+				"sceneId" : "1234",
+				"address" : "42 Wallaby Way, Sydney",
+				"active" : True,
+				"assigned_organizations" : ["EMS"],
+				"description" : "This is a description",
+				"latitude" : "56.3423342",
+				"longitude" : "34.342346",
+				"time" : "00:00",
+				"title" : "Structure Fire",
+				}
+				)
+			# sceneid,address,active,assigned_organization,description,latitude,longitude,time,title
 
 	return render_template('incidents.html', NAV=nav["header"], INCIDENTS=INCIDENTS)
 
