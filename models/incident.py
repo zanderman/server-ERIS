b58492ad0f153b718b4a72697a619ec9ace17ad3
Alexander DeRieux <acd1797@vt.edu>
2016-11-10 19:16:10 -0500

    Added status field 'active' to incident object

--- models/incident.py
+++ models/incident.py
@@ -1,16 +1,19 @@
 
 class Incident(object):
 	"""docstring for Incident"""
-	def __init__(self, ID, description, address, latitude, longitude, time, title, organizations):
+	def __init__(self, ID, description, address, active, latitude, longitude, time, title, organizations):
 		self.id = ID
 		self.description = description
 		self.address = address
+		self.active = active
 		self.latitude = latitude
 		self.longitude = longitude
 		self.time = time
 		self.title = title
 		self.organizations = organizations
 
+	def __eq__(self, other):
+		return self.id == other.id
 
 
 
